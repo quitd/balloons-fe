55751380e837dc5d1131d41e14461cd3e41dcfea
quitd <degrou99@gmail.com>
2022-03-02 11:37:02 +0700

    loading idk why not

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,28 @@
 import '../styles/globals.css';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme, Spinner, Center } from '@chakra-ui/react';
+import {useRouter} from 'next/router';
+import {useState, useEffect} from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [l, setL] = useState(false);
+  const router = useRouter();
+
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", url => setL(url !== router.pathname));
+    router.events.on("routeChangeComplete", () => setL(false));
+    router.events.on("routeChangeError", () => setL(false));
+  }, [router]);
+
   return <ChakraProvider theme={extendTheme({
-  components: {
-    Divider: {
-      baseStyle: {
-        my: 3
-      }
+    components: {
+      Divider: {
+        baseStyle: {
+          my: 3
+        }
+      },
     },
-  },
-})}><Component {...pageProps} /></ChakraProvider>
+  })}>{l ? <Center w="100vw" h="100vh"><Spinner size="xl" thickness="3px" /></Center> : <Component {...pageProps} />}</ChakraProvider>
 }
 
 export default MyApp
