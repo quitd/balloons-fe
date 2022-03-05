fe0883ae265334158a0d24783dd3b84ff1be4b5c
quitd <degrou99@gmail.com>
2022-03-05 15:45:54 +0700

    test

--- pages/api/rescan.ts
+++ pages/api/rescan.ts
@@ -1,10 +1,13 @@
-export default async function(_, res) {
-  await fetch('https://api.github.com/repos/quitd/balloons-be/actions/workflows/21097005/dispatches', {
+import scan from '../../scan';
+
+export default async function Scan(_, res) {
+  /*await fetch('https://api.github.com/repos/quitd/balloons-be/actions/workflows/21097005/dispatches', {
     method: 'post',
     body: JSON.stringify({ref: 'master'}),
     headers: {
       Authorization: 'token '+process.env.GH
     }
-  })
+  })*/
+  await scan();
   res.end();
 }
